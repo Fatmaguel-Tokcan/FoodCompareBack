61940ad2b259f585b1957f08a752aafb6654790e
s0583236 <seda.delikaya@student.htw-berlin.de>
2023-06-26 09:10:26 +0200

    Update

--- app.py
+++ app.py
@@ -3,43 +3,51 @@ import pandas as pd
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
-CORS(app, resources={r"/": {"origins": "http://localhost:8081", "headers": "", "methods": "*"}})
-
+CORS(app)
 
 df = pd.read_csv('RAW_recipes.csv')
 print(df.head())
 
-
-@app.route('/gericht', methods=['GET'])
+@app.route('/api/gericht', methods=['GET'])
 def get_gericht():
-    gericht_id = request.args.get('id')
-    gericht_data = df.loc[df['id'] == int(gericht_id)]
+    name = request.args.get('name')
+    if name is None:
+        return jsonify({'error': 'Gerichtsname nicht angegeben.'}), 400
+
+    gericht_data = df.loc[df['name'] == name]
     if gericht_data.empty:
         return jsonify({'error': 'Gericht nicht gefunden.'}), 404
-    
+
     gericht = {
-        'id': gericht_id,
-        'name': gericht_data['name'].values[0],
+        'name': name,
         'minutes': gericht_data['minutes'].values[0],
         'tags': gericht_data['tags'].values[0],
         'nutrition': gericht_data['nutrition'].values[0],
         'steps': gericht_data['steps'].values[0],
         'description': gericht_data['description'].values[0],
         'ingredients': gericht_data['ingredients'].values[0],
-
     }
+
     return jsonify(gericht)
 
-@app.route('/zutaten', methods=['GET'])
-def get_zutaten():
-    gericht_id = request.args.get('id')
-    zutaten_data = df.loc[df['id'] == int(gericht_id), 'ingredients'].values[0]
-    if not zutaten_data:
-        return jsonify({'error': 'Gericht nicht gefunden.'}), 404
-    
-    zutaten = zutaten_data.split(',')
-    return jsonify(zutaten)
+@app.route('/api/gerichte', methods=['GET'])
+def get_gerichte():
+    gerichte_data = df.to_dict('records')
+    if len(gerichte_data) == 0:
+        return jsonify({'error': 'Keine Gerichte gefunden.'}), 404
 
-if __name__ == '_main_':
-    app.run(debug=True)
+    return jsonify(gerichte_data)
+
+@app.route('/api/zutaten/<name>', methods=['GET'])
+def get_zutaten(name):
+    zutaten_data = df.loc[df['name'] == name, 'ingredients'].values
+    if len(zutaten_data) == 0:
+        return jsonify({'error': 'Gericht nicht gefunden.'}), 404
+
+    zutaten = zutaten_data[0].split(',')
+    response = jsonify(zutaten)
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    return response
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
